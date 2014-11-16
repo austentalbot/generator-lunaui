f08d5d5a76181fe802e6bf6fee8a9b6c7495cf23
Austen Talbot <austentalbot@gmail.com>
2014-11-16 11:57:23 -0800

    Added option to create new folder

--- app/index.js
+++ app/index.js
@@ -1,44 +1,78 @@
 var yeoman = require('yeoman-generator');
 
 var ComponentGenerator = yeoman.generators.Base.extend({
-  promptUser: function() {
-    var done = this.async();
-    console.log(this.yeoman);
+  prompting: {
+    promptUser: function() {
+      var done = this.async();
+      console.log(this.yeoman);
 
-    var prompts = [{
-      name: 'name',
-      message: 'What is your component\'s name? (in all lower case with spaces)',
-      default: 'component'
-    }];
+      var prompts = [{
+        name: 'name',
+        message: 'What is your component\'s name? (in all lower case with spaces)',
+        default: 'component'
+      }, {
+        type: 'confirm',
+        name: 'newFolder',
+        message: 'Do you want to create this component in a new folder?',
+        default: false
+      }];
 
-    this.prompt(prompts, function (props) {
-      //name as entered with spaces
-      this.componentname = props.name;
-      //name snake_cased 
-      this.component_name = props.name.replace(/ /g, '_');
-      //name capitalized and camelCased
-      this.ComponentName = props.name.replace(/\b./g, function(x) {
-        return x.toUpperCase();
-      }).replace(/ /g, '');
-      //name camelCased
-      this.componentName = this.ComponentName.charAt(0).toLowerCase() + this.ComponentName.slice(1);
+      this.prompt(prompts, function (props) {
 
-      done();
-    }.bind(this));
+        var truthy = {
+          't': true,
+          'T': true,
+          'true': true,
+          'True': true,
+          'TRUE': true,
+          'y': true,
+          'Y': true,
+          'yes': true,
+          'Yes': true,
+          'YES': true
+        };
+
+        //name as entered with spaces
+        this.componentname = props.name;
+        //name snake_cased 
+        this.component_name = props.name.replace(/ /g, '_');
+        //name capitalized and camelCased
+        this.ComponentName = props.name.replace(/\b./g, function(x) {
+          return x.toUpperCase();
+        }).replace(/ /g, '');
+        //name camelCased
+        this.componentName = this.ComponentName.charAt(0).toLowerCase() + this.ComponentName.slice(1);
+
+        //set up new folder path
+        this.newFolder = '';
+        if (props.newFolder && props.newFolder in truthy) {
+          this.newFolder = this.component_name + '/';
+        }
+
+        done();
+      }.bind(this));
+    }
   },
-  copyMainFiles: function() {
-    var context = { 
-      componentname: this.componentname,
-      component_name: this.component_name,
-      ComponentName: this.ComponentName,
-      componentName: this.componentName
-    };
+  writing: {
+    app: function() {
+      if (this.newFolder.length) {
+        this.dest.mkdir(this.component_name);
+      }
+    },
+    templates: function() {
+      var context = { 
+        componentname: this.componentname,
+        component_name: this.component_name,
+        ComponentName: this.ComponentName,
+        componentName: this.componentName
+      };
 
-    this.copy('_component.ts', this.component_name + '.ts');
- 
-    this.template('_component_spec.ts', this.component_name + '_spec.ts', context);
+      this.copy('_component.ts', this.newFolder + this.component_name + '.ts');
+   
+      this.template('_component_spec.ts', this.newFolder + this.component_name + '_spec.ts', context);
 
-    this.template('__component.scss', '_' + this.component_name + '.scss', context);
+      this.template('__component.scss', this.newFolder + '_' + this.component_name + '.scss', context);
+    }
   }
 
 });
